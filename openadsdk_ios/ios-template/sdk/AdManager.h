2906aa12b563f1e7b0b982173ea92b05a36b28b4
leeyang <leeyang1990@gmail.com>
2020-03-06 16:19:22 +0800

    re add ios

---
+++ openadsdk_ios/ios-template/sdk/AdManager.h
@@ -0,0 +1,13 @@
+//
+//  AdManager.h
+//  ios-template
+//
+//  Created by ani on 2020/3/6.
+//  Copyright © 2020 egret. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdManager : NSObject
+
+@end
